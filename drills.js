ab0612b4ad2e67f2f6a06c4d689766e5f3e607bb
William Wong <wwong1987@gmail.com>
2019-01-14 11:54:16 -0800

    added try catch

--- drills.js
+++ drills.js
@@ -1,4 +1,7 @@
 function getYearOfBirth(age) {
+    if (age < 0) {
+        throw new Error('Age can not be negative');
+    }
     return 2019 - age;
 }
 
@@ -7,5 +10,8 @@ function createGreeting(name, age) {
     return `Hi, my name is William and I'm 5 years old`;
 }
 
-const greeting1 = createGreeting();
-console.log(greeting1);
+try {
+    const greeting1 = createGreeting('Michael', -10);
+} catch(error) {
+    throw new Error('Age not valid');
+}
